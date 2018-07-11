ff5996d075eaa13c151c18fe61b6bdfea1549cd2
shannonbee <shannonbenyamin@gmail.com>
2018-07-11 18:12:57 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -40,6 +40,7 @@ puts student_names[7]
 def first_second_and_third_students
  puts ["Adele","Beyonce","Cardi B"]
 end
-puts first_second_and_third_students
 end 
+puts first_second_and_third_students
+
 
